7d89df2981037c31e9476c48b0873502a3901f0e
Jakub Jankowski <jankowskijakub59@gamil.com>
2024-04-25 19:21:23 +0200

    Dodaj pliki projektów.

---
+++ JJLab04/Form1.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace JJLab04
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBoxResult = new System.Windows.Forms.PictureBox();
+            this.pictureBoxOriginal = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.Wgraj = new System.Windows.Forms.Button();
+            this.Zeruj = new System.Windows.Forms.Button();
+            this.Zapisz = new System.Windows.Forms.Button();
+            this.ResetT = new System.Windows.Forms.Button();
+            this.Red = new System.Windows.Forms.Button();
+            this.Green = new System.Windows.Forms.Button();
+            this.Blue = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxResult)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxOriginal)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBoxResult
+            // 
+            this.pictureBoxResult.Location = new System.Drawing.Point(545, 70);
+            this.pictureBoxResult.Name = "pictureBoxResult";
+            this.pictureBoxResult.Size = new System.Drawing.Size(308, 307);
+            this.pictureBoxResult.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBoxResult.TabIndex = 0;
+            this.pictureBoxResult.TabStop = false;
+            // 
+            // pictureBoxOriginal
+            // 
+            this.pictureBoxOriginal.Location = new System.Drawing.Point(77, 70);
+            this.pictureBoxOriginal.Name = "pictureBoxOriginal";
+            this.pictureBoxOriginal.Size = new System.Drawing.Size(308, 307);
+            this.pictureBoxOriginal.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBoxOriginal.TabIndex = 1;
+            this.pictureBoxOriginal.TabStop = false;
+            this.pictureBoxOriginal.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(192, 54);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Obraz bazowy";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(673, 54);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(84, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Obraz wynikowy";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // Wgraj
+            // 
+            this.Wgraj.Location = new System.Drawing.Point(77, 383);
+            this.Wgraj.Name = "Wgraj";
+            this.Wgraj.Size = new System.Drawing.Size(75, 23);
+            this.Wgraj.TabIndex = 5;
+            this.Wgraj.Text = "wgraj";
+            this.Wgraj.UseVisualStyleBackColor = true;
+            this.Wgraj.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // Zeruj
+            // 
+            this.Zeruj.Location = new System.Drawing.Point(169, 383);
+            this.Zeruj.Name = "Zeruj";
+            this.Zeruj.Size = new System.Drawing.Size(75, 23);
+            this.Zeruj.TabIndex = 6;
+            this.Zeruj.Text = "Zeruj";
+            this.Zeruj.UseVisualStyleBackColor = true;
+            // 
+            // Zapisz
+            // 
+            this.Zapisz.Location = new System.Drawing.Point(77, 425);
+            this.Zapisz.Name = "Zapisz";
+            this.Zapisz.Size = new System.Drawing.Size(75, 23);
+            this.Zapisz.TabIndex = 7;
+            this.Zapisz.Text = "Zapisz";
+            this.Zapisz.UseVisualStyleBackColor = true;
+            this.Zapisz.Click += new System.EventHandler(this.Zapisz_Click);
+            // 
+            // ResetT
+            // 
+            this.ResetT.Location = new System.Drawing.Point(169, 425);
+            this.ResetT.Name = "ResetT";
+            this.ResetT.Size = new System.Drawing.Size(75, 23);
+            this.ResetT.TabIndex = 8;
+            this.ResetT.Text = "Reset";
+            this.ResetT.UseVisualStyleBackColor = true;
+            this.ResetT.Click += new System.EventHandler(this.Reset_Click);
+            // 
+            // Red
+            // 
+            this.Red.Location = new System.Drawing.Point(310, 395);
+            this.Red.Name = "Red";
+            this.Red.Size = new System.Drawing.Size(75, 23);
+            this.Red.TabIndex = 9;
+            this.Red.Text = "R";
+            this.Red.UseVisualStyleBackColor = true;
+            this.Red.Click += new System.EventHandler(this.Red_Click);
+            // 
+            // Green
+            // 
+            this.Green.Location = new System.Drawing.Point(391, 395);
+            this.Green.Name = "Green";
+            this.Green.Size = new System.Drawing.Size(75, 23);
+            this.Green.TabIndex = 10;
+            this.Green.Text = "G";
+            this.Green.UseVisualStyleBackColor = true;
+            this.Green.Click += new System.EventHandler(this.Green_Click);
+            // 
+            // Blue
+            // 
+            this.Blue.Location = new System.Drawing.Point(472, 395);
+            this.Blue.Name = "Blue";
+            this.Blue.Size = new System.Drawing.Size(75, 23);
+            this.Blue.TabIndex = 11;
+            this.Blue.Text = "B";
+            this.Blue.UseVisualStyleBackColor = true;
+            this.Blue.Click += new System.EventHandler(this.Blue_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(903, 572);
+            this.Controls.Add(this.Blue);
+            this.Controls.Add(this.Green);
+            this.Controls.Add(this.Red);
+            this.Controls.Add(this.ResetT);
+            this.Controls.Add(this.Zapisz);
+            this.Controls.Add(this.Zeruj);
+            this.Controls.Add(this.Wgraj);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBoxOriginal);
+            this.Controls.Add(this.pictureBoxResult);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxResult)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxOriginal)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBoxResult;
+        private System.Windows.Forms.PictureBox pictureBoxOriginal;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.Button Wgraj;
+        private System.Windows.Forms.Button Zeruj;
+        private System.Windows.Forms.Button Zapisz;
+        private System.Windows.Forms.Button ResetT;
+        private System.Windows.Forms.Button Red;
+        private System.Windows.Forms.Button Green;
+        private System.Windows.Forms.Button Blue;
+    }
+}
+
---
+++ JJLab04/Form1.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace JJLab04
+{
+    public partial class Form1 : Form
+    {
+        Bitmap obrazekOrginal;
+        Bitmap obrazekCopy;
+        int wysokosc;
+        int szerokosc;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            OpenFileDialog okienko = new OpenFileDialog();
+            okienko.Filter = "JPG|*.jpg|PNG|*.png|BMP|*.bmp|TIFF|*.tif";
+            if (okienko.ShowDialog() == DialogResult.OK && okienko.CheckFileExists == true)
+                pictureBoxOriginal.Image = Image.FromFile(okienko.FileName);
+            obrazekOrginal = (Bitmap)pictureBoxOriginal.Image;
+            obrazekCopy = obrazekOrginal;
+            wysokosc = obrazekCopy.Height;
+            szerokosc = obrazekCopy.Width;
+            pictureBoxResult.Image = obrazekCopy;
+
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Red_Click(object sender, EventArgs e)
+        {
+            Color pxl;
+            for(int i = 0; i < wysokosc; i++)
+            {
+                for(int j = 0; j < szerokosc; j++)
+                {
+                    pxl = Color.FromArgb(255, 0, 0);
+                    obrazekCopy.SetPixel(i, j, pxl);
+                }
+            }
+            pictureBoxResult.Image = obrazekCopy;   
+
+        }
+
+        private void Green_Click(object sender, EventArgs e)
+        {
+            Color pxl;
+            for (int i = 0; i < wysokosc; i++)
+            {
+                for (int j = 0; j < szerokosc; j++)
+                {
+                    pxl = Color.FromArgb(0, 255, 0);
+                    obrazekCopy.SetPixel(i, j, pxl);
+                }
+            }
+            pictureBoxResult.Image = obrazekCopy;
+        }
+
+        private void Blue_Click(object sender, EventArgs e)
+        {
+            Color pxl;
+            for (int i = 0; i < wysokosc; i++)
+            {
+                for (int j = 0; j < szerokosc; j++)
+                {
+                    pxl = Color.FromArgb(0, 0, 255);
+                    obrazekCopy.SetPixel(i, j, pxl);
+                }
+            }
+            pictureBoxResult.Image = obrazekCopy;
+        }
+
+        private void Zapisz_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog okienko = new SaveFileDialog();
+            okienko.Filter = "JPG|*.jpg|PNG|*.png|BMP|*.bmp|TIFF|*.tif";
+            if (okienko.ShowDialog() == DialogResult.OK)
+            {
+                pictureBoxResult.Image.Save(okienko.FileName, ImageFormat.Jpeg);
+                MessageBox.Show("Zapisano plik");
+            }
+
+        }
+
+        private void Reset_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
